40e1065aa344ff03df2e0137b56d0801d3fd03f8
ikane12 <ikane@emich.edu>
2019-02-23 22:49:51 -0500

    This is a commit
    I am sorry for breaking the last 2

---
+++ src/components/DashButton.vue
@@ -0,0 +1,21 @@
+<template>
+   <v-container>
+   <a :href="link">
+      <img :src="source" contain height = "200"></img>
+      <span>{{text}}</span>
+   </a>
+   </v-container>
+</template>
+
+<script>
+
+
+   export default {
+      props: ['source', 'text', 'link']
+   }
+
+</script>
+
+<style>
+
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,18 +1,26 @@
 <template>
-    <div>
-        <h1>HELLO USER!</h1>
-        <button @click='logOut'>Log out</button>
-    </div>
+   <div>
+      <DashButton :source = "require('../assets/settings.svg')" text = "Settings" link = "Settings" />
+
+   </div>
 </template>
 
 <script>
-    export default {
-        name: "Dashboard",
-        methods: {
-            logOut() {
-                this.$firebase.auth().signOut();
-            }
-        }
+   import DashButton from '@/components/DashButton.vue'
+   import { Component, Vue } from 'vue-property-decorator';
+
+   @Component({
+     components: {
+        DashButton,
+     },
+   })
+
+
+
+    export default class Dashboard extends Vue{
+      logOut() {
+         this.$firebase.auth().signOut();
+      }
     }
 </script>
 
