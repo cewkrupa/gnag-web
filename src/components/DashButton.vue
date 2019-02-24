4ca8e7b8bfd100eb3413b2c655ca8e6bfeeaed9f
ikane12 <ikane@emich.edu>
2019-02-24 00:41:59 -0500

    Dashboard buttons done poorly
    I don't know how the grid system works in vuetify, Connor help me please

--- src/components/DashButton.vue
+++ src/components/DashButton.vue
@@ -1,10 +1,21 @@
 <template>
    <v-container>
    <a :href="link">
-      <img :src="source" contain height = "200"></img>
-      <span>{{text}}</span>
+
+      <v-flex>
+         <v-card>
+            <v-img class = "" :src="source" contain height = "200"></v-img>
+            <v-layout class="justify-center">
+               <v-card-title primary-title>
+                  <div>
+                     <h3 class="headline mb-0">{{text}}</h3>
+                  </div>
+               </v-card-title>
+            </v-layout>
+         </v-card>
+      </v-flex>
    </a>
-   </v-container>
+</v-container>
 </template>
 
 <script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,21 +1,46 @@
 <template>
-   <div>
-      <DashButton :source = "require('../assets/settings.svg')" text = "Settings" link = "Settings" />
+   <v-container fluid>
+    <v-layout column>
+      <v-flex>
+               <v-layout row >
+
+                  <v-flex xs:3>
+               <dashbutton :source = "require('../assets/settings.svg')" text = "Settings" link = "Settings" />
+                  </v-flex>
+                  <v-flex xs:3>
+               <dashbutton :source = "require('../assets/groups.svg')" text = "Groups" link = "Settings" />
+                  </v-flex>
+                  <v-flex xs:3>
+               <dashbutton :source = "require('../assets/messages.svg')" text = "Messages" link = "Settings" />
+                  </v-flex>
+
+
+                  <v-flex xs:3>
+               <dashbutton :source = "require('../assets/noose.svg')" text = "Suicide Letters" link = "Settings" />
+                  </v-flex>
+               <v-flex xs:3>
+               <dashbutton :source = "require('../assets/marriage.svg')" text = "Marriage Certificates" link = "Settings" />
+                  </v-flex>
+               <v-flex xs:3>
+               <dashbutton :source = "require('../assets/notes.svg')" text = "Personal Notes" link = "Settings" />
+                  </v-flex>
+               </v-layout>
+            </v-flex>
+         </v-layout>
+      </v-container>
 
    </div>
 </template>
 
 <script>
-   import DashButton from '@/components/DashButton.vue'
+   import dashbutton from '@/components/DashButton.vue'
    import { Component, Vue } from 'vue-property-decorator';
 
    @Component({
      components: {
-        DashButton,
+        dashbutton,
      },
    })
-
-
 
     export default class Dashboard extends Vue{
       logOut() {
